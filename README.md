64ba943ada2db34ca30b2d96c33195572cf0e2e4
bakankaya <batakankarakaya@gmail.com>
2024-04-05 18:42:39 +0300

    Added a new function to solve Ascii char problem

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # weather_app
 Simple weather app with HTML
 
-The font is [Digital-7](https://www.1001fonts.com/users/style-7/) By Style-7 from [1001fonts](https://www.1001fonts.com/users/style-7/).
+The font is [Digital-7](https://www.1001fonts.com/digital-7-font.html) By Style-7 from [1001fonts](https://www.1001fonts.com/users/style-7/).
 
 Favicon is [Clouds](https://icons8.com/icon/QdG76489aLZo/clouds) icon from [Icons8](https://icons8.com).
 
 
+- localization 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import convertToASCII from "./convertToASCII.js";
+
 /* Creating Date Format */
 
 const date = new Date();
@@ -18,14 +20,14 @@ document.querySelector(".date span").textContent = date.toLocaleDateString('en-E
 const cityQuery = document.querySelector(".location input");
 const city = cityQuery.value;
 const APIKEY = "a127619b02228ad2ee53bda273b6dabb";
-const BASE_URL = ` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
 
 fetch(BASE_URL)
   .then((resp)=> resp.json())
   .then((data) =>{
     if (data.cod === 200){
         document.querySelector(".image").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-        document.querySelector(".cityName").textContent = data.name;
+        document.querySelector(".cityName").textContent = `${convertToASCII(data.name)}`;
         document.querySelector(".description").textContent = data.weather[0].description;
         document.querySelector(".temp").textContent = `${Math.round(data.main.temp)} °C `;
         document.querySelector(".speed").textContent = `${data.wind.speed} m/s`;
@@ -33,13 +35,13 @@ fetch(BASE_URL)
         document.querySelector(".feeling").textContent = `${Math.round(data.main.feels_like)} 'C`;
         document.querySelector(".sunrise").textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString("tr-TR",optionsTime);
         document.querySelector(".sunset").textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString("tr-TR",optionsTime);
-//cityQuery.value = " ";
+        cityQuery.value = "";
     } else {
         document.querySelector(".cityName").textContent = "City not Found";
         const clearing = document.querySelectorAll(".info");
         clearing.forEach((e) => {
           e.innerText= "--";
         });
-        cityQuery.value = " ";
+        cityQuery.value = "";
     }
   })
