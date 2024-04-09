cf44b4a266fe53308f4703fc32ec128917fda9dd
bakankaya <batakankarakaya@gmail.com>
2024-04-09 12:40:03 +0300

    Added auto-location, functional search, gui improvements and ReadMe

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # weather_app
-Simple weather app with HTML
 
-The font is [Digital-7](https://www.1001fonts.com/digital-7-font.html) By Style-7 from [1001fonts](https://www.1001fonts.com/users/style-7/).
+![weather_app_Screenshot](./src/weather_app.png)
 
-Favicon is [Clouds](https://icons8.com/icon/QdG76489aLZo/clouds) icon from [Icons8](https://icons8.com).
+Simple weather app made with JavaScript to show the weather conditions in the desired location.
+
+The design is inspired from old digital watches *like Casio FW-91W*.
+
+There is also react component version of it, which can be found at [weather_component](https://github.com/bakankaya/weather_component).
+
+To use you need to get your own API keys from listed services (*for free*).
+
+---
+Used Sources:
+
+- Weather information is from [OpenWeather](https://openweathermap.org/api).
+
+- Automatic location finding is made with [Geoapify](https://www.geoapify.com/).
+
+- The font is [Digital-7](https://www.1001fonts.com/digital-7-font.html) By Style-7 from [1001fonts](https://www.1001fonts.com/users/style-7/).
+
+- Favicon is [Clouds](https://icons8.com/icon/QdG76489aLZo/clouds) icon from [Icons8](https://icons8.com).
 
 
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import convertToASCII from "./convertToASCII.js";
+import convertToASCII from "./src/convertToASCII.js";
+
+/* API KEYS */
+
+const weatherAPI = "";
+const locationAPI = "";
 
 /* Creating Date Format */
 
@@ -16,32 +21,59 @@ const optionsTime = {
 
 document.querySelector(".date span").textContent = date.toLocaleDateString('en-EN', optionsDate);
 
-/* Default City */
-const cityQuery = document.querySelector(".location input");
-const city = cityQuery.value;
-const APIKEY = "";
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
 
-fetch(BASE_URL)
-  .then((resp)=> resp.json())
-  .then((data) =>{
-    if (data.cod === 200){
-        document.querySelector(".image").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-        document.querySelector(".cityName").textContent = `${convertToASCII(data.name)}`;
-        document.querySelector(".description").textContent = data.weather[0].description;
-        document.querySelector(".temp").textContent = `${Math.round(data.main.temp)} °C `;
-        document.querySelector(".speed").textContent = `${data.wind.speed} m/s`;
-        document.querySelector(".humidity").textContent = `${data.main["humidity"]} %`;
-        document.querySelector(".feeling").textContent = `${Math.round(data.main.feels_like)} 'C`;
-        document.querySelector(".sunrise").textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString("tr-TR",optionsTime);
-        document.querySelector(".sunset").textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString("tr-TR",optionsTime);
-        cityQuery.value = "";
-    } else {
-        document.querySelector(".cityName").textContent = "City not Found";
-        const clearing = document.querySelectorAll(".info");
-        clearing.forEach((e) => {
-          e.innerText= "--";
-        });
-        cityQuery.value = "";
-    }
-  })
+/* Getting The Default City from Geoapify */
+
+const location_URL = `https://api.geoapify.com/v1/ipinfo?apiKey=${locationAPI}`
+
+fetch(location_URL)
+.then(resp => resp.json())
+.then((userLocationData) => {
+  getWeather(userLocationData.city.name)
+});
+
+
+/* Getting Weather Data from Openweather */
+
+function getWeather(cityName) {
+
+  const weather_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPI}&units=metric`;
+
+  fetch(weather_URL)
+    .then((resp)=> resp.json())
+    .then((data) =>{
+      if (data.cod === 200){
+          document.querySelector(".image").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+          document.querySelector(".cityName").textContent = `${convertToASCII(data.name)}`;
+          document.querySelector(".description").textContent = data.weather[0].description;
+          document.querySelector(".temp").textContent = `${Math.round(data.main.temp)} °C `;
+          document.querySelector(".speed").textContent = `${data.wind.speed} m/s`;
+          document.querySelector(".humidity").textContent = `${data.main["humidity"]} %`;
+          document.querySelector(".feeling").textContent = `${Math.round(data.main.feels_like)} 'C`;
+          document.querySelector(".sunrise").textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString("tr-TR",optionsTime);
+          document.querySelector(".sunset").textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString("tr-TR",optionsTime);
+      } else {
+          document.querySelector(".cityName").textContent = "City not Found";
+          const clearing = document.querySelectorAll(".info");
+          clearing.forEach((e) => {
+            e.innerText= "--";
+          });
+      }
+    })
+};
+
+
+/* Search Button */
+
+const btn = document.getElementById('search');
+btn.addEventListener('click', searchWeather)
+  function searchWeather(){
+    getWeather(document.getElementById("input").value);
+    document.getElementById("input").value = "";
+  };
+
+const bar = document.getElementById("input");
+bar.addEventListener('keyup',
+  (e) =>{if(e.code ==='Enter'){
+    searchWeather()}});
+
